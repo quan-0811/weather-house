f363b44ea0845dbb089c2b1a9ae831e16db2c489
nguyendat0309 <dat.nt225481@sis.hust.edu.vn>
2025-12-15 09:07:21 +0700

    Add model training script

---
+++ src/dags/training_dag.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'data-engineer',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=10),
+}
+
+with DAG(
+    'weather_model_training',
+    default_args=default_args,
+    description='Retrains the Weather Forecast Model (Weekly)',
+    schedule_interval='@daily',  # Runs once a day
+    start_date=datetime(2023, 11, 30),
+    catchup=False,
+    tags=['spark', 'mlops', 'training'],
+) as dag:
+
+    # --- TASK: MODEL TRAINING ---
+    # Re-trains the model using the accumulated Gold data
+    # We give it more memory (1GB) because training requires loading data into RAM
+    train_model = BashOperator(
+        task_id='train_model',
+        bash_command="""
+        docker exec spark-master /opt/spark/bin/spark-submit \
+        --master spark://spark-master:7077 \
+        --deploy-mode client \
+        --conf spark.cores.max=2 \
+        --conf spark.executor.memory=1024m \
+        --conf spark.driver.memory=1024m \
+        /opt/spark/src/ml/train_model.py
+        """
+    )
